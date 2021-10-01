399177e803f1d77222d270a10f04b0130c7235de
Miguel Edward A. Rosal <miguelrosal@exploretale.com>
2021-10-01 12:41:01 +0800

    Workout Buttons

--- lib/widget_screen.dart
+++ lib/widget_screen.dart
@@ -1,6 +1,8 @@
 import "package:flutter/material.dart";
 import 'package:homefit/widgets/constants/constant.dart';
+import 'package:homefit/widgets/constants/grid.dart';
 import 'package:homefit/widgets/widgets.dart';
+import 'package:homefit/widgets/workout_button.dart';
 
 class HFWidgetScreen extends StatefulWidget {
   const HFWidgetScreen({Key? key, required this.title}) : super(key: key);
@@ -18,53 +20,68 @@ class _HFWidgetScreenState extends State<HFWidgetScreen> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-        HFText.title("Homefit"),
-        SizedBox(height: HFGrid.small),
-        HFText(
-          'This is a demo Text',
-          color: HFColor.scorpion,
-          size: HFTextSize.small,
-          weight: HFTextWeight.bold,
-          style: HFTextStyle.normal,
-        ),
-        SizedBox(
-          height: HFGrid.large,
-        ),
-        HFText(
-          'This is a demo2 Text',
-          color: HFColor.orange,
-          size: HFTextSize.large,
-          weight: HFTextWeight.extraBold,
-          style: HFTextStyle.normal,
-        ),
-        SizedBox(
-          height: HFGrid.large,
-        ),
-        HFText(
-          'This is a demo2 Text',
-          color: HFColor.blue,
-          size: HFTextSize.large,
-          weight: HFTextWeight.extraBold,
-          style: HFTextStyle.normal,
-        ),
-        SizedBox(
-          height: HFGrid.large,
-        ),
-        HFAppBar(
-          title: "Home Workout",
-        ),
-        SizedBox(
-          height: HFGrid.large,
-        ),
-        HFAppBar(
-          title: "ABS BEGGINER",
-        ),
-        SizedBox(
-          height: HFGrid.large,
-        ),
-      ])),
+      body: SingleChildScrollView(
+        child: Center(
+            child:
+                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          HFText.title("Homefit"),
+          SizedBox(height: HFGrid.small),
+          HFText(
+            'This is a demo Text',
+            color: HFColor.scorpion,
+            size: HFTextSize.small,
+            weight: HFTextWeight.bold,
+            style: HFTextStyle.normal,
+          ),
+          SizedBox(
+            height: HFGrid.large,
+          ),
+          HFText(
+            'This is a demo2 Text',
+            color: HFColor.orange,
+            size: HFTextSize.large,
+            weight: HFTextWeight.extraBold,
+            style: HFTextStyle.normal,
+          ),
+          SizedBox(
+            height: HFGrid.large,
+          ),
+          HFText(
+            'This is a demo2 Text',
+            color: HFColor.blue,
+            size: HFTextSize.large,
+            weight: HFTextWeight.extraBold,
+            style: HFTextStyle.normal,
+          ),
+          SizedBox(
+            height: HFGrid.large,
+          ),
+          HFAppBar(
+            title: "Home Workout",
+          ),
+          SizedBox(
+            height: HFGrid.large,
+          ),
+          HFAppBar(
+            title: "ABS BEGGINER",
+          ),
+          SizedBox(
+            height: HFGrid.large,
+          ),
+          HFWorkoutButtons(category: "Beginner", title: "ABS Beginner"),
+          SizedBox(
+            height: HFGrid.large,
+          ),
+          HFWorkoutButtons(category: "Intermediate", title: "ABS Intermediate"),
+          SizedBox(
+            height: HFGrid.large,
+          ),
+          HFWorkoutButtons(category: "Advance", title: "ABS Advance"),
+          SizedBox(
+            height: HFGrid.large,
+          )
+        ])),
+      ),
     );
   }
 }
---
+++ lib/widgets/workout_button.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+import 'package:homefit/widgets/constants/grid.dart';
+import 'package:homefit/widgets/text.dart';
+import 'constants/constant.dart';
+
+class HFWorkoutButtons extends StatelessWidget {
+  final String category;
+  final String title;
+  final VoidCallback? onTap;
+
+  const HFWorkoutButtons({
+    Key? key,
+    required this.category,
+    required this.title,
+    this.onTap,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: ClipRRect(
+        borderRadius: BorderRadius.all(Radius.circular(HFGrid.small)),
+        child: Container(
+          width: MediaQuery.of(context).size.width * .85,
+          decoration: BoxDecoration(
+              color: HFColor.blue,
+              borderRadius: BorderRadius.all(Radius.circular(HFGrid.small)),
+              boxShadow: [
+                BoxShadow(
+                    offset: Offset(0, 17),
+                    blurRadius: 17,
+                    spreadRadius: -23,
+                    color: HFColor.gray)
+              ]),
+          child: Padding(
+            padding: const EdgeInsets.all(HFGrid.medium),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                HFText(
+                  title,
+                  color: Colors.white,
+                  weight: HFTextWeight.bold,
+                  size: HFTextSize.large,
+                ),
+                SizedBox(
+                  height: HFGrid.xxxxxLarge,
+                ),
+                _icons()
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _icons() {
+    if (category.toUpperCase() == 'BEGINNER') {
+      return Row(
+        children: [
+          Icon(
+            Icons.bolt,
+            color: HFColor.orange,
+          ),
+          Icon(
+            Icons.bolt,
+            color: HFColor.gray,
+          ),
+          Icon(
+            Icons.bolt,
+            color: HFColor.gray,
+          )
+        ],
+      );
+    } else if (category.toUpperCase() == 'INTERMEDIATE') {
+      return Row(
+        children: [
+          Icon(
+            Icons.bolt,
+            color: HFColor.orange,
+          ),
+          Icon(
+            Icons.bolt,
+            color: HFColor.orange,
+          ),
+          Icon(
+            Icons.bolt,
+            color: HFColor.gray,
+          )
+        ],
+      );
+    } else {
+      return Row(
+        children: [
+          Icon(
+            Icons.bolt,
+            color: HFColor.orange,
+          ),
+          Icon(
+            Icons.bolt,
+            color: HFColor.orange,
+          ),
+          Icon(
+            Icons.bolt,
+            color: HFColor.orange,
+          )
+        ],
+      );
+    }
+  }
+}
