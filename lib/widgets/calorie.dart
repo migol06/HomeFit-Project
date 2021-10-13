d3c188e9cc50530e08d395ad493de78919ab670e
Miguel Edward A. Rosal <miguelrosal@exploretale.com>
2021-10-13 14:36:07 +0800

    CalorieWidget

---
+++ lib/widgets/calorie.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:homefit/widgets/constants/grid.dart';
+import 'package:homefit/widgets/text.dart';
+
+import 'constants/constant.dart';
+
+class HFCalories extends StatefulWidget {
+  final String calorieTitle;
+  final int calorieCalc;
+  const HFCalories(
+      {Key? key, required this.calorieTitle, required this.calorieCalc})
+      : super(key: key);
+
+  @override
+  _HFCaloriesState createState() => _HFCaloriesState();
+}
+
+class _HFCaloriesState extends State<HFCalories> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: HFColor.orange,
+      width: MediaQuery.of(context).size.width * .60,
+      child: Padding(
+        padding: const EdgeInsets.all(HFGrid.medium),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            HFText(widget.calorieTitle,
+                color: Colors.white, size: HFTextSize.medium),
+            Spacer(),
+            HFText(widget.calorieCalc.toString(),
+                color: Colors.white, size: HFTextSize.medium)
+          ],
+        ),
+      ),
+    );
+  }
+}
