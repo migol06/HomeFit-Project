9e6e6ffd2e37b01f04a47ba7cdd12eff712ddf5d
Miguel Edward A. Rosal <miguelrosal@exploretale.com>
2021-11-29 12:02:54 +0800

    Update the UI

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -35,37 +35,7 @@ class HFHomeScreen extends StatelessWidget {
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Align(
-                          alignment: Alignment.topRight,
-                          child: Container(
-                              height: 52,
-                              width: 52,
-                              decoration: BoxDecoration(
-                                  color: HFColor.lightBlue,
-                                  shape: BoxShape.circle),
-                              child: PopupMenuButton(
-                                  icon: Icon(
-                                    Icons.more_vert_outlined,
-                                    color: Colors.white,
-                                  ),
-                                  itemBuilder: (context) => [
-                                        PopupMenuItem(
-                                          child: ListTile(
-                                            title: Text('About us'),
-                                            onTap: () {
-                                              showDialog(
-                                                  context: context,
-                                                  builder: (_) => AlertDialog(
-                                                        title: Text('About Us'),
-                                                        content: Text(
-                                                            'Welcome To HomeFit' +
-                                                                '\n\nHomeFit is a Professional Educational Platform. Here we will provide you with only interesting content, which you will like very much. We\'re dedicated to providing you the best of Educational, with a focus on dependability and Workout Routines. '),
-                                                      ));
-                                            },
-                                          ),
-                                        )
-                                      ])),
-                        ),
+                        SizedBox(height: HFGrid.large),
                         HFText("HOMEFIT",
                             color: Colors.white,
                             size: HFTextSize.xxLarge,
@@ -116,11 +86,21 @@ class HFHomeScreen extends StatelessWidget {
                                   width: HFGrid.large,
                                 ),
                                 HFHomeButtons(
-                                    textAlignment: MainAxisAlignment.center,
-                                    title: "About Us",
-                                    color: HFColor.gray,
-                                    width:
-                                        MediaQuery.of(context).size.width * .40)
+                                  textAlignment: MainAxisAlignment.center,
+                                  title: "About Us",
+                                  color: HFColor.gray,
+                                  width:
+                                      MediaQuery.of(context).size.width * .40,
+                                  onTap: () {
+                                    showDialog(
+                                        context: context,
+                                        builder: (_) => AlertDialog(
+                                              title: Text('About Us'),
+                                              content: Text('Welcome To HomeFit' +
+                                                  '\n\nHomeFit is a Professional Educational Platform. Here we will provide you with only interesting content, which you will like very much. We\'re dedicated to providing you the best of Educational, with a focus on dependability and Workout Routines. '),
+                                            ));
+                                  },
+                                )
                               ],
                             )
                           ],
